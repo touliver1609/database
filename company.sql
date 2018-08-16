eb5220d87189f4561417cd03757e5d1310b41d4a
Phat <phat.nguyen@vexere.com>
2018-08-16 14:56:17 +0700

    phat create table historyofhireseats

--- company.sql
+++ company.sql
@@ -1,6 +1,6 @@
-
+--delete 1 table
 drop table public.companies;
-
+--create 1  table voi cac thuoc tinh thiet lap primary key
 CREATE TABLE public.companies (
 	id serial NOT NULL,
 	"name" varchar NOT NULL,
@@ -13,11 +13,12 @@ CREATE TABLE public.companies (
 	"updatedAt" date NULL,
 	CONSTRAINT company_pk PRIMARY KEY (id)
 )
-
+-- Xem tat ca cac du lieu cua table
 select * from public.companies;
 
+-- insert du lieu moi vao table  
 insert into public.companies values(1,'VXR','123abc','01216101714','113adv','abc@abc.com',true);
-
+-- update du lieu cua 1 hang da ton tai theo field id 
 update public.companies set name='VXR1', "taxID"='abc'  where id = 1;
-
+-- delete du lieu o 1 hang da ton tai theo field id
 delete from public.companies where id = 1;
