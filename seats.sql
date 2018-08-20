3ed3e23690315130e39d39395ee6f2228ee4714d
Le Nguyen Thuy Quynh Nhu <nhu.le@vexere.com>
2018-08-20 10:14:22 +0700

    change all file sql

--- seats.sql
+++ seats.sql
@@ -1,27 +1,22 @@
-drop table public."seats";
-create type rentType as enum('person','company');
-create table public."seats"(
- 	id int4 NOT NULL,
-	"column" int4 NOT NULL,
-	"row" int4 NOT NULL,
-	"parkingLostId" int4 NOT NULL,
+--DROP TABLE public."seats";
+--DROP type status;
+CREATE TYPE status AS enum('available','unavailable');
+CREATE TABLE public."seats"(
+	"id" serial NOT NULL,
+	"idSeat" varchar NOT NULL UNIQUE,
 	"status" status NOT NULL,
-	"rentedDate" date NOT NULL,
-	"expirationDate" date NOT NULL,
-	"rentedType" rentType NOT NULL,
-	"rentedId" int4 NOT NULL,
-	"carNumeber" varchar NOT NULL,
-	"createdAt" date NOT NULL,
-	"updatedAt" date NOT NULL,
+	"column" INTEGER NOT NULL,
+	"row" INTEGER NOT NULL,
+	"parkingLostId" integer NOT NULL,
+	"createdAt"  date NOT NULL,
+	"updatedAt"  date NOT NULL,
 	CONSTRAINT seats_pk PRIMARY KEY (id),
-	CONSTRAINT seats_parkinglosts_fk FOREIGN KEY ("parkingLostId") REFERENCES public."parkingLosts"(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT seats_companies_fk FOREIGN KEY ("rentedId") REFERENCES public."companies"(id) ON UPDATE CASCADE ON DELETE cascade,
-	CONSTRAINT seats_persons_fk FOREIGN KEY ("rentedId") REFERENCES public."persons"(id) ON UPDATE CASCADE ON DELETE cascade
-	--check ("rentedType"::text='company') CONSTRAINT seats_companies_fk FOREIGN KEY ("rentedId") REFERENCES public."companies"(id) ON UPDATE CASCADE ON DELETE cascade,
-	--check ("rentedType"::text='person') CONSTRAINT seats_persons_fk FOREIGN KEY ("rentedId") REFERENCES public."persons"(id) ON UPDATE CASCADE ON DELETE cascade
-);--check ("rentedType"::text='company') 
-insert into public."seats" values (1,1,1,1,'hired','2018-05-05','2018-10-05','person',1,'49F1-01902','2018-08-16','2018-08-16');
-insert into public."seats" values (2,2,1,1,'available','2018-05-05','2018-10-05','company',1,'49F1-01902','2018-08-16','2018-08-16');
-insert into public."seats" values (3,3,1,1,'available','2018-05-05','2018-10-05','company',2,'49F1-01902','2018-08-16','2018-08-16');
-select * from public."seats";
-select * from public."seats" where public."seats"."rentedType"='company' ;
+	constraint seats_areas_fk foreign key ("parkingLostId") references public."areas"(id)
+);
+
+INSERT INTO public."seats" VALUES ('1','P11','available','1','1','1','2018-08-18','2018-08-18');
+INSERT INTO public."seats" VALUES ('2','P12','unavailable','1','2','1','2018-08-18','2018-08-18');
+--INSERT INTO public."seats" VALUES ('3','P13','abc','1','3','100'); => fail
+SELECT * FROM public."seats";
+-- query with condition area is parkingLost
+select * from public."seats" where "status"='available';
